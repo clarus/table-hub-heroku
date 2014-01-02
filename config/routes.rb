880820455c2f8027de16297a5d1a4f0dde0d0365
Guillaume Claret <dev@clarus.me>
2014-01-02 18:45:42 +0100

    SignUp page added

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ SampleApp::Application.routes.draw do
   match '/help', to: 'static_pages#help', via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
   match '/about', to: 'static_pages#about', via: 'get'
+  match '/signup', to: 'users#new', via: 'get'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -29,7 +29,7 @@ describe "Static pages" do
   describe "About page" do
     before { visit about_path }
 
-    it { should have_content("About Us") }
-    it { should have_title(full_title("About Us")) }
+    it { should have_content("About") }
+    it { should have_title(full_title("About")) }
   end
 end
