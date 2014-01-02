afe7000f8a2d70189139aa536db24a2455c1a27d
Guillaume Claret <dev@clarus.me>
2014-01-03 00:51:38 +0100

    User model: better validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -37,7 +37,7 @@ describe User do
   describe "when email format is invalid" do
     it "should be invalid" do
       addresses = %w[user@foo,com user_at_foo.org example.user@foo.
-        foo@bar_baz.com foo@bar+baz.com]
+        foo@bar_baz.com foo@bar+baz.com foo@bar..com]
       addresses.each do |invalid_address|
         @user.email = invalid_address
         expect(@user).not_to be_valid
@@ -88,5 +88,15 @@ describe User do
   describe "with a password that's too short" do
     before { @user.password = @user.password_confirmation = "a" * 5 }
     it { should be_invalid }
+  end
+
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      expect(@user.reload.email).to eq mixed_case_email.downcase
+    end
   end
 end
