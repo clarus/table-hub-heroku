4b6d0fbb78f35785527838e9ec858349044a07ab
Guillaume Claret <dev@clarus.me>
2014-01-02 22:50:31 +0100

    User model: true uniqueness for email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  before_save { self.email = self.email.downcase }
   validates :name, presence: true, length: { maximum: 128 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
