8a1372074fb20f2376980e587eddb8389c018107
Guillaume Claret <dev@clarus.me>
2014-01-02 22:09:34 +0100

    Basic user model test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before { @user = User.new name: "Me", email: "me@email.com" }
+  subject { @user }
+
+  it { should respond_to(:name) }
+  it { should respond_to(:email) }
 end
