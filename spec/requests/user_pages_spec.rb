7dad9344bd930fa68453e17e815b724e937948f6
Guillaume Claret <dev@clarus.me>
2014-01-03 18:38:26 +0100

    Some improvments

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -14,6 +14,27 @@ describe "User pages" do
       it "should not create an user" do
         expect { click_button submit }.not_to change(User, :count)
       end
+
+      describe "after empty submission" do
+        before { click_button submit }
+
+        it { should have_title("Sign Up") }
+        it { should have_content("error") }
+      end
+
+      describe "after submission with wrong email" do
+        before do
+          fill_in "Name", with: "Guillaume Claret"
+          fill_in "Email", with: "guillaume@email"
+          fill_in "Password", with: "secret"
+          fill_in "Confirmation", with: "secret"
+          click_button submit
+        end
+
+        it { should have_title("Sign Up") }
+        it { should have_content("1 error") }
+        it { should have_content("Email is invalid") }
+      end
     end
 
     describe "with valid information" do
@@ -26,6 +47,14 @@ describe "User pages" do
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
+      end
+
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by email: "guillaume@email.com" }
+
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: "Welcome") }
       end
     end
   end
