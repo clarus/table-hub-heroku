289e7162218677a0cff73a76685f7426cb7409f2
Guillaume Claret <dev@clarus.me>
2014-01-04 22:01:05 +0100

    Signout added

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,6 +6,13 @@ module SessionsHelper
     self.current_user = user
   end
 
+  def sign_out
+    current_user.update_attribute(:remember_token,
+      User.encrypt(User.new_remember_token))
+    cookies.delete(:remember_token)
+    self.current_user = nil
+  end
+
   def signed_in?
     !current_user.nil?
   end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -34,6 +34,11 @@ describe "Authentication" do
       it { should have_link("Profile", href: user_path(user)) }
       it { should have_link("Sign out", href: signout_path) }
       it { should_not have_link("Sign in") }
+
+      describe "followed by signout" do
+        before { click_link "Sign out" }
+        it { should have_link("Sign in") }
+      end
     end
   end
 end
