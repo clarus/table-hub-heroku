e8a95d8404a6ed6f43a77f7765c0ef68b33c5668
Guillaume Claret <dev@clarus.me>
2014-01-05 02:14:52 +0100

    Must be logged in to edit user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:edit, :update]
+
   def new
     @user = User.new
   end
@@ -35,5 +37,12 @@ class UsersController < ApplicationController
   private
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
+    def signed_in_user
+      unless signed_in?
+        flash[:warning] = "Please sign in."
+        redirect_to signin_path
+      end
     end
 end
